0836a6c6162f55060520726f32b631faf2cc8fe1
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-09-27 12:24:27 -0300

    construindo dados  e card

---
+++ src/componentes/Card/card.tsx
@@ -0,0 +1,61 @@
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+
+type CardProps = {
+  id: number;
+  name: string;
+  ingredients?: string;
+  price: number;
+  image?: string;
+};
+
+function CardOptions({
+  id, name, ingredients = '', price, image = '' }: CardProps) {
+  return (
+    <Card
+      key={ id }
+      sx={ {
+        maxWidth: 345,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      } }
+    >
+      {image && (
+        <CardMedia
+          sx={ { height: 140 } }
+          image={ image }
+          title={ name }
+        />
+      )}
+
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        {ingredients && (
+          <Typography variant="body2" color="text.secondary">
+            {ingredients}
+          </Typography>
+        )}
+      </CardContent>
+      <CardActions
+        sx={ {
+          display: 'flex',
+          justifyContent: 'end',
+          alignItems: 'center',
+          padding: '1rem',
+        } }
+      >
+        <Typography variant="body1" color="text.secondary">
+          {`R$: ${price.toFixed(2).replace('.', ',')}`}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default CardOptions;
--- src/pages/Lanches/index.tsx
+++ src/pages/Lanches/index.tsx
@@ -1,7 +1,124 @@
+import { Typography } from '@mui/material';
+import PaoDeHamburguer from '../../Data/Lanches/pao-de-hamburguer';
+import PaoFrances from '../../Data/Lanches/pao-frances';
+import HotDogs from '../../Data/Lanches/hot-dog';
+import LanchesNoPrato from '../../Data/Lanches/Lanches-no-prato';
+
+
+import CardOptions from '../../componentes/Card/card';
+
+import styles from './lanches.module.css';
+
 function Lanches() {
   return (
-    <div>
-      <h1>Lanches</h1>
+    <div className={ styles.mainContainer }>
+      <div
+        className={ styles.titleContainer }
+      >
+        <h2>
+          Pão de Hamburguer
+        </h2>
+      </div>
+
+      <div
+        className={ styles.gridContainer }
+      >
+        <div
+          className={ styles.grid }
+        >
+          {PaoDeHamburguer.map((item) => (
+            <CardOptions
+              key={ item.id }
+              name={ item.name }
+              ingredients={ item.ingredients }
+              price={ item.price }
+              image={ item.image }
+              id={ item.id }
+            />
+          ))}
+        </div>
+      </div>
+
+      <div
+        className={ styles.titleContainer }
+      >
+        <h2>
+          Pão Francês
+        </h2>
+      </div>
+
+      <div
+        className={ styles.gridContainer }
+      >
+        <div
+          className={ styles.grid }
+        >
+          {PaoFrances.map((item) => (
+            <CardOptions
+              key={ item.id }
+              name={ item.name }
+              ingredients={ item.ingredients }
+              price={ item.price }
+              image={ item.image }
+              id={ item.id }
+            />
+          ))}
+        </div>
+      </div>
+
+      <div
+        className={ styles.titleContainer }
+      >
+        <h2>
+          Hot Dogs
+        </h2>
+      </div>
+
+      <div
+        className={ styles.gridContainer }
+      >
+        <div
+          className={ styles.grid }
+        >
+          {HotDogs.map((item) => (
+            <CardOptions
+              key={ item.id }
+              name={ item.name }
+              ingredients={ item.ingredients }
+              price={ item.price }
+              image={ item.image }
+              id={ item.id }
+            />
+          ))}
+        </div>
+      </div>
+
+      <div
+        className={ styles.titleContainer }
+      >
+        <h2>
+          Lanches no Prato
+        </h2>
+      </div>
+
+      <div
+        className={ styles.gridContainer }
+      >
+        <div
+          className={ styles.grid }
+        >
+          {LanchesNoPrato.map((item) => (
+            <CardOptions
+              key={ item.id }
+              name={ item.name }
+              ingredients={ item.ingredients }
+              price={ item.price }
+              image={ item.image }
+              id={ item.id }
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
